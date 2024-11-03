b78fdba4ae257d826fc7bcf2241091f987e5c641
SayyedAquib <sayyed.aquib89@gmail.com>
2024-11-03 17:30:59 +0530

    removed github pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/url-shortener/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
