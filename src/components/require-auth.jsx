c58156804d539d9951132f1c4ef9654e6bb3ddd5
Sayyed Aquib <72184419+SayyedAquib@users.noreply.github.com>
2024-11-09 19:46:33 +0530

    Update require-auth.jsx

--- src/components/require-auth.jsx
+++ src/components/require-auth.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import { UrlState } from "@/context";
+import { UrlState } from "@/Context";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { BarLoader } from "react-spinners";
